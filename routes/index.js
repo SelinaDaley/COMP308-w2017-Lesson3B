89cff313c790421b47c3c91a6d2a2e72fc059627
SelinaDaley <sdmd6@hotmail.com>
2017-01-28 15:52:43 -0500

    migrated to Express and EJS

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+let express = require('express');
+let router = express.Router();
+
+// global route variables
+let currentDate = new Date();
+currentDate = currentDate.toLocaleTimeString();
+
+/* GET home page. wildcard */
+router.get('/', (req, res, next) => {
+  res.render('index', { 
+    title: 'Express', 
+    date: currentDate
+  });
+});
+
+/* GET about page. */
+router.get('/about', (req, res, next) => {
+  res.render('index', { 
+    title: 'About', 
+    date: currentDate
+  });
+});
+
+module.exports = router;
